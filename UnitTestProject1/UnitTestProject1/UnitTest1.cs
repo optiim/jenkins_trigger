0f334d4f3a8c3c423b60970bbf00d4931dd67557
OptiimPC2 <OptiimPC2@DESKTOP-T2VD7A9>
2017-02-08 16:15:04 +0300

    Dev Test

--- UnitTestProject1/UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTestProject1/UnitTest1.cs
@@ -1,5 +1,10 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
 
 namespace UnitTestProject1
 {
@@ -7,11 +12,43 @@ namespace UnitTestProject1
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestMethod1(string[] args)
         {
 
-            int demo;
-
-          
+            int r, m1, m2, m3, t;
+            float p;
+            string n;
+            Console.WriteLine("Enter Roll Number :");
+            r = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter User Name :");
+            n = Console.ReadLine();
+            Console.WriteLine("Subject1 : ");
+            m1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Subject2 : ");
+            m2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Subject3 : ");
+            m3 = Convert.ToInt32(Console.ReadLine());
+            t = m1 + m2 + m3;
+            p = t / 3.0f;
+            Console.WriteLine("Total : " + t);
+            Console.WriteLine("Percentage : " + p);
+            if (p >= 35 && p < 50)
+            {
+                Console.WriteLine("Result is C");
+            }
+            if (p >= 50 && p <= 60)
+            {
+                Console.WriteLine("Result is B");
+            }
+            if (p > 60 && p <= 80)
+            {
+                Console.WriteLine("Result is A");
+            }
+            if (p > 80 && p <= 100)
+            {
+                Console.WriteLine("Result is A+");
+            }
+            Console.ReadLine();
+        }
+      }
     }
-}
